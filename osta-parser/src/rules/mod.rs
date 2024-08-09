22c3ebc9e81407d3628d5193be13dd898c4059de
Artik <xairosan@gmail.com>
2024-08-09 19:53:48 +0200

    Parse if-else statements

--- osta-parser/src/rules/mod.rs
+++ osta-parser/src/rules/mod.rs
@@ -1,5 +1,6 @@
 pub mod expr;
 mod stmt;
+mod flow;
 
 #[cfg(test)]
 pub mod tests {
@@ -18,7 +19,9 @@ pub mod tests {
         macro_rules! assert_ast {
             ($func:expr, $input:expr, $nodes:pat, $datas:pat) => {{
                 let (mut tokenizer, mut ast_builder) = (Tokenizer::new($input), AstBuilder::new());
-                let result = $func(&mut tokenizer, &mut ast_builder).unwrap();
+                let _result = $func(&mut tokenizer, &mut ast_builder).unwrap();
+
+                dbg!(&ast_builder.ast);
 
                 // NOTE(cdecompilador): using matches! here may make some literal matching painful
                 // for example we can't write !0 inside
