a3f8bb4a4caa596569d36b51ace3828858d15eea
JohanVonElectrum <joanestradium@gmail.com>
2024-08-09 14:28:52 +0200

    fix fallible! macro bug with args

--- osta-parser/src/lib.rs
+++ osta-parser/src/lib.rs
@@ -7,7 +7,7 @@ pub mod parser;
 macro_rules! fallible {
     ($func:expr, $tokenizer:expr, $builder:expr $(, $arg:expr)*) => {
         $func($tokenizer, $builder, $($arg),*).map_err(|err| {
-            builder.rollback(&err).expect("rollback failed");
+            $builder.rollback(&err).expect("rollback failed");
             err
         })
     };
